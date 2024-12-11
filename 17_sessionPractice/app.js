545177b246874edc1ffef9557e91b6f88e0487bb
lychee-55 <zylee503@naver.com>
2024-12-11 16:07:18 +0900

    세션 리더님 풀이 업로드

--- 17_sessionPractice/app.js
+++ 17_sessionPractice/app.js
@@ -19,64 +19,91 @@ app.use(session({
     }
 }))
 
+// 임시 DB
+const userInfo = {
+    userId: "cocoa",
+    userPw: "1234"
+}
+
 app.get("/",(req,res)=>{
     // 로그인이 안된 유저라면 > { isLogin : false }
     // 로그인 된 유저라면 > { isLogin : true, user:user }
-    if(req.session.name){
-        res.render("index",{isLogin:true,userId:req.session.name})
+    if(req.session.user){
+        const user = req.session.user
+        res.render("index",{isLogin: true, user: user})
     }else{
         res.render("index",{isLogin:false})
     }
 })
 
 app.get("/login",(req,res)=>{
-    res.render("login")
+    // 로그인 된 유저라면 메인페이지로
+    // 로그인이 안된 유저라면 로그인페이지로
+    const user = req.session.user
+    if(user){
+        res.redirect("/")
+    }else{
+        res.render("login")
+    }
 })
 
-const userInfo = {
-    userId: "cocoa",
-    userPw: "1234"
-}
 
 // POST /login  -- 실제 로그인 진행, 일치하면 로그인 (세션 연결)
 app.post("/login",(req,res)=>{
-    console.log(req.body) //{ id: '1111', pw: '1111' }
-    // 세션의 user 라는 키를 추가하여 userId값을 value로 전달
-    
-    const {userId, userPw} = userInfo
-    
-    if(req.body.id===userId && req.body.pw===userPw){
-        // res.render("index",{ isLogin: true, userId: req.body.id})
-        req.session.name= userId;
-        console.log(req.session)   
-    /* Session {
-    cookie: {
-    path: '/',
-    _expires: 2024-12-10T03:04:29.274Z,
-    originalMaxAge: 600000,
-    httpOnly: true
-    },
-    name: 'cocoa'
-    } */
+    // 로그인 여부 판단
+    if(
+        userInfo.userId === req.body.id &&
+        userInfo.userPw === req.body.pw
+    ){
+        // console.log("로그인 가능한 user")
+        // 세션 생성
+        req.session.user = req.body.id
+        console.log(req.session)
+        /*
+        Session {
+            cookie: {
+              path: '/',
+              _expires: 2024-12-11T02:25:18.547Z,
+              originalMaxAge: 600000,
+              httpOnly: true
+            },
+            user: 'cocoa'
+          } 
+        */
         res.redirect("/")
-        // res.send({id:req.sessionID,name:req.session.name})
     }else{
-        console.log("아이디 또는 패스워드가 맞지 않습니다!")
-        res.send(`<h2>아이디 또는 패스워드가 맞지 않습니다!</h2>`)
+        // console.log("로그인 불가능한 user")
+        res.send(`
+            <script>
+                alert("아이디 또는 비밀번호가 틀렸어요. 다시 시도하세요")
+                document.location.href="/login"
+            </script>
+            `)
     }
-    // res.send(req.body.id)
+    // res.send("응답완료!")
 })
 
 // GET /logout -- destroy, 실제 로그인 진행(세션 삭제)
 app.get("/logout",(req,res)=>{
-    req.session.destroy((err)=>{
-        if(err) throw err
-
-        // res.send("세션 삭제 완료")
-        console.log("session 삭제 완료!")
-        console.log(req.session)
-        res.redirect("/")
-    })
+    // 세션 삭제
+    console.log("GET /logout",req.session)
+    const user = req.session.user
+    if(user){
+        // 로그인 된 유저라면 >> 로그아웃 시켜주기 
+        req.session.destroy((err)=>{
+            if(err) throw err
+            res.redirect("/")
+        })
+    }else{
+        // 로그인 안 된 유저(세션이 만료된 유저, 세션 생성 10분 후)
+        res.send(`
+            <script>
+                alert("이미 세션이 만료 되었어요!");
+                document.location.href="/";
+            </script>
+            `)
+    }
+    
 })
 
 app.listen(PORT,()=>{
