880997e535e8ff6cc5c66bff7a308bde70ea0f4b
lychee-55 <zylee503@naver.com>
2025-01-13 17:44:41 +0900

    react CSS

---
+++ 23-react-style/src/App.jsx
@@ -0,0 +1,12 @@
+import BasicCss from './components/BasicCss';
+import './style/basic_css.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicCss />
+    </div>
+  );
+}
+
+export default App;
---
+++ 23-react-style/src/components/ModuleCss.jsx
@@ -0,0 +1,33 @@
+import classNames from 'classnames';
+import style from '../style/style.module.css';
+import Names from 'classnames/bind';
+
+export default function ModuleCss() {
+  const setting = Names.bind(style); // className={classNames(style.first, style.second)}이렇게 불러오기 복잡할 때 편하게 사용.
+  return (
+    <div className={style.container}>
+      <h4>module css 적용</h4>
+      <div className={style.box2}>
+        <div>안녕하세요?</div>
+      </div>
+      <br />
+      <div className={`${style.first} ${style.second}`}>
+        클래스를 여러개 지정하는 방법1 (템플릿 리터럴)
+      </div>
+      {/* 
+      [1,2,3,4,5].join("-") >> 1-2-3-4-5
+      ["first","second"].join(" ") >> "first second"
+      */}
+      <div className={[style.first, style.second].join(' ')}>
+        클래스를 여러개 지정하는 방법2 (배열과 join이용)
+      </div>
+      {/* classnames는 함수 이기에 () 필요 */}
+      <div className={classNames(style.first, style.second)}>
+        클래스를 여러개 지정하는 방법3 (classnames 모듈 설치)
+      </div>
+      <div className={setting('first', 'second')}>
+        clasnames 모듈 사용 더 해보기
+      </div>
+    </div>
+  );
+}
